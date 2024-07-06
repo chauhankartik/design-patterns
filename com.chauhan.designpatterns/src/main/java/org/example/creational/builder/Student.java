66cc06a71c6bdf5062f74af15e1d8a042d01f46d
chauhan <kartikchauhan987@gmail.com>
2024-07-06 20:02:50 +0530

    added builder pattern

---
+++ com.chauhan.designpatterns/src/main/java/org/example/creational/builder/Student.java
@@ -0,0 +1,29 @@
+package org.example.creational.builder;
+
+import java.util.List;
+
+public class Student {
+    int rollNumber;
+    int age;
+    String name;
+    String fatherName;
+    String motherName;
+    List<String> subjects;
+
+    public Student(StudentBuilder builder){
+        this.rollNumber = builder.rollNumber;
+        this.age = builder.age;
+        this.name = builder.name;
+        this.fatherName = builder.fatherName;
+        this.motherName = builder.motherName;
+        this.subjects = builder.subjects;
+    }
+
+    public String toString(){
+        return ""+ " roll number: " + this.rollNumber +
+                " age: " + this.age +
+                " name: " + this.name +
+                " father name: " + this.fatherName +
+                " mother name: " + this.motherName;
+    }
+}
